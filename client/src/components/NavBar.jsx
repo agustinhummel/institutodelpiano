29aed168ea1176a085bb3878aff3535db61396bc
miguelbel00 <miguelabeltranp@gmail.com>
2023-02-24 08:25:48 -0500

    fix error navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -18,7 +18,7 @@ const NavBar = () => {
           <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-8 cursor-pointer md:hidden'>
           <ion-icon name={open ? 'close': 'menu' }></ion-icon>
           </div>
-            <ul className={`md:flex md:items-center md:pb-0 h-screen   absolute md:static bg-sky-300 md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 opacity-100':'top-[-490px]'} md:opacity-100 opacity-0`}>
+            <ul className={`md:flex md:items-center md:pb-0 h-screen md:h-full  absolute md:static bg-sky-300 md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 opacity-100':'top-[-490px]'} md:opacity-100 opacity-0`}>
               <li className='md:ml-8 text-xl md:my-0 my-7 text-gray-700 hover:text-yellow-300 cursor-pointer duration-500' onClick={()=>setOpen(!open)}>
                 <Link to='/'>
                   Home
