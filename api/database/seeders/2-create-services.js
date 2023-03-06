d5dbcfdc31531578170e9b642a1102c4437d71fb
agustinhummel <agustinhummel7@gmail.com>
2023-03-06 17:30:14 -0300

    fixed change

--- api/database/seeders/2-create-services.js
+++ api/database/seeders/2-create-services.js
@@ -4,7 +4,17 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert(
       "Services",
-      [{
+      [
+      {
+          name: "servico de acompañamiento escolar",
+          description:"Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. ",
+          image:["https://img.freepik.com/vector-premium/bandera-conceptos-palabra-kinesiologia-monitoreo-muscular-trastornos-movimiento_106317-1548.jpg"],
+          price: 1500,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          deletedAt: null
+      },  
+      {
         name: "kinesiologia",
         description:"Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. ",
         image:["https://img.freepik.com/vector-premium/bandera-conceptos-palabra-kinesiologia-monitoreo-muscular-trastornos-movimiento_106317-1548.jpg"],
