a6505c518a3c5fab9dd4bfe67731a6fabd38db4a
miguelbel00 <miguelabeltranp@gmail.com>
2023-03-15 21:57:34 -0500

    add instagram

--- client/src/Pages/Vacante.jsx
+++ client/src/Pages/Vacante.jsx
@@ -15,12 +15,13 @@ const Vacante = () => {
     }, [])
 
     return (
-        <section class="text-gray-600 body-font mt-20">
-        <div class="container px-5 py-16 mx-auto">
-            <div class="flex flex-wrap w-full mb-20 flex-col items-center text-center">
-            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Puestos de trabajo disponibles</h1>
+        <section className="text-gray-600 body-font mt-20">
+        <div className="container px-5 py-16 mx-auto">
+            <div className="flex flex-wrap w-full mb-20 flex-col items-center text-center">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Puestos de trabajo disponibles</h1>
             </div>
-            {
+            { allVacantes.length > 0 
+                ?
                 allVacantes?.map((v, i)=> (
                     <Jobs
                         key={i}
@@ -28,6 +29,13 @@ const Vacante = () => {
                         description={v.description}
                     />
                 ))
+                :   <div  className="flex">
+                        <div className="w-fit justify-center mx-auto p-4">
+                            <div className="border border-gray-200 p-6 rounded-lg shadow">
+                                <h2 className="text-lg text-gray-900 font-medium title-font mb-2 text-center">Proximamente estaremos publicando mas vacantes</h2>
+                            </div>
+                        </div>
+                    </div>
             }
         </div>
         <form 
@@ -35,49 +43,48 @@ const Vacante = () => {
             method='POST'
             encType="multipart/form-data"
             >
-        <div className="text-gray-600 body-font relative">
-            <div class="container px-5 pb-20 mx-auto">
-                <div class="flex flex-col text-center w-full mb-12">
-                <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Contactate con nosotros</h1>
-                <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Si quieres sumarte a nuestro equipo de trabajo, no dudes en dejarnos tu informacion.</p>
+        <div className="text-gray-600 body-font relative border-2  md:max-w-2xl lg:max-w-5xl md:mx-auto my-10 pt-8">
+            <div className="container px-5 pb-20 mx-auto">
+                <div className="flex flex-col text-center w-full mb-12">
+                    <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Contactate con nosotros</h1>
+                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Si quieres sumarte a nuestro equipo de trabajo, no dudes en dejarnos tu informacion.</p>
                 </div>
-                <div class="lg:w-1/2 md:w-2/3 mx-auto">
-                <div class="flex flex-wrap -m-2">
-                    <div class="p-2 w-1/2">
-                    <div class="relative">
-                        <label for="nombre" class="leading-7 text-sm text-gray-600">Nombre</label>
-                        <input type="text" id="nombre" name="nombre" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                <div className="lg:w-1/2 md:w-2/3 mx-auto">
+                    <div className="flex flex-wrap -m-2">
+                        <div className="p-2 w-1/2">
+                            <div className="relative">
+                                <label for="nombre" className="leading-7 text-sm text-gray-600">Nombre</label>
+                                <input type="text" id="nombre" name="nombre" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                            </div>
+                        </div>
+                        <div className="p-2 w-1/2">
+                            <div className="relative">
+                                <label for="correo" className="leading-7 text-sm text-gray-600">Email</label>
+                                <input type="email" id="correo" name="correo" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                            </div>
+                        </div>
+                        <div className="p-2 w-full">
+                            <div className="relative">
+                                <label for="message" className="leading-7 text-sm text-gray-600">Presentacion</label>
+                                <textarea id="mensaje" name="mensaje" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                            </div>
+                        </div>
+                        <div className="p-2 w-full">
+                            <div className="relative">
+                                <label for="vacante" className="leading-7 text-sm text-gray-600">Vacante a la que aplicas</label>
+                                <input type="text" id="vacante" name="vacante" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                            </div>
+                        </div>
+                        <div className="p-2 w-full">
+                            <div className="relative">
+                                <label for="cv" className="leading-7 text-sm text-gray-600">CV</label>
+                                <input type="file" id="cv" name="cv" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                            </div>
+                        </div>
+                        <div className="p-2 w-full">
+                            <button className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Enviar</button>
+                        </div>
                     </div>
-                    </div>
-                    <div class="p-2 w-1/2">
-                    <div class="relative">
-                        <label for="correo" class="leading-7 text-sm text-gray-600">Email</label>
-                        <input type="email" id="correo" name="correo" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
-                    </div>
-                    </div>
-                    <div class="p-2 w-full">
-                    <div class="relative">
-                        <label for="message" class="leading-7 text-sm text-gray-600">Presentacion</label>
-                        <textarea id="mensaje" name="mensaje" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
-                    </div>
-                    </div>
-                    <div class="p-2 w-full">
-                    <div class="relative">
-                        <label for="vacante" class="leading-7 text-sm text-gray-600">Vacante a la que aplicas</label>
-                        <input type="text" id="vacante" name="vacante" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
-                    </div>
-                    </div>
-                    <div class="p-2 w-full">
-                    <div class="relative">
-                        <label for="cv" class="leading-7 text-sm text-gray-600">CV</label>
-                        <input type="file" id="cv" name="cv" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
-                    </div>
-                    </div>
-                    <div class="p-2 w-full">
-                    <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Enviar</button>
-                    </div>
-                    
-                </div>
                 </div>
             </div>
         </div>
--- client/src/components/Jobs.jsx
+++ client/src/components/Jobs.jsx
@@ -3,18 +3,15 @@ import React from "react";
 const Jobs = ({name, description}) => {
 
     return (
-
-            <div  className="flex flex-wrap mx-auto">
-            <div className="w-full p-4">
-                <div className="border border-gray-200 p-6 rounded-lg shadow">
-                <h2 className="text-lg text-gray-900 font-medium title-font mb-2">{name.charAt(0).toUpperCase() + name.toLowerCase().slice(1)}</h2>
-                <p className="leading-relaxed text-base">{new DOMParser().parseFromString(description, "text/html").body.textContent}</p>
+            <div  className="flex ">
+                <div className="max-w-5xl mx-auto w-full p-4">
+                    <div className="border border-gray-200 p-6 rounded-lg shadow">
+                        <h2 className="text-lg text-gray-900 font-medium title-font mb-2">{name.charAt(0).toUpperCase() + name.toLowerCase().slice(1)}</h2>
+                        <p className="leading-relaxed text-base">{new DOMParser().parseFromString(description, "text/html").body.textContent}</p>
+                    </div>
                 </div>
             </div>
-            </div>
-
     )
-
 }
 
 export default Jobs
