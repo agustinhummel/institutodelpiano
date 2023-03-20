4aa9a98f86a3e231d7940c288c213de7082a7e44
agustinhummel <agustinhummel7@gmail.com>
2023-03-20 19:12:14 -0300

    merge

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { Link } from 'react-router-dom'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer.jsx';
 import Home from "./Pages/Home.jsx"
@@ -9,7 +10,6 @@ import Blog from './Pages/Blog';
 import AdminPage from './AdminPage/Admin.jsx';
 import Protected from './components/Protected';
 import ProtectedAdmin from './components/ProtectedAdmin';
-import NotFoundPage from './pages/NotFoundPage.jsx';
 import CreateProfessional from './adminPage/CreateProfessional.jsx';
 import CreatePost from './AdminPage/CreatePost.jsx';
 import CreateService from './adminPage/CreateService.jsx';
@@ -67,3 +67,21 @@ function App() {
 }
 
 export default App
+
+
+const NotFoundPage = () => {
+  return (
+    <section className="flex items-center h-screen p-16  dark:text-dark-color">
+		<div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+			<div className="max-w-md text-center">
+				<h2 className="mb-8 font-extrabold text-9xl dark:text-gray-600">
+					<span className="sr-only">Error</span>404
+				</h2>
+				<p className="text-2xl font-semibold md:text-3xl">Lo Siento, No hemos podido encontrar esta pagina</p>
+				<p className="mt-4 mb-8 dark:text-gray-400">Pero no te preocupes, puedes encontrar muchas otras cosas en nuestro inicio </p>
+				<Link to={'/'} className="px-8 py-3 font-semibold rounded bg-main-color text-dark-color">Volver a Inicio</Link>
+			</div>
+		</div>
+	</section>
+  )
+}
